96f9afab8c56a9e049ebb8654ffa0dd414acf7e6
Vadim Radzih <iphonevadim2003@gmail.com>
2023-07-30 10:48:42 +0300

    Added rabbitmq

--- src/everyday_joke/config.py
+++ src/everyday_joke/config.py
@@ -4,12 +4,16 @@ from everyday_joke.bot.config import TgBot
 from everyday_joke.bot.config import load_config as load_bot_config
 from everyday_joke.infra.db.config import Database
 from everyday_joke.infra.db.config import load_config as load_db_config
+from everyday_joke.infra.rabbitmq.config import RabbitMQ
+from everyday_joke.infra.rabbitmq.config import \
+    load_config as load_rabbitmq_config
 
 
 @dataclass
 class Config:
     tg_bot: TgBot
     db: Database
+    rabbitmq: RabbitMQ
     debug: bool = False
 
 
@@ -17,5 +21,6 @@ def load_config():
     return Config(
         tg_bot=load_bot_config(),
         db=load_db_config(),
+        rabbitmq=load_rabbitmq_config(),
         debug=True,
     )
