6e144edeb110c8d1152cdf4be715648f67eacacd
Vadim Radzih <iphonevadim2003@gmail.com>
2023-08-12 21:54:56 +0300

    Added command to subscribe

--- src/everyday_joke/bot/handlers/subscribe.py
+++ src/everyday_joke/bot/handlers/subscribe.py
@@ -1,11 +1,27 @@
 from aiogram import F, Router
+from aiogram.filters import Command
 from aiogram.types import CallbackQuery, Message, User
 
 from everyday_joke.bot import locales
 from everyday_joke.bot.keyboards import callback
+from everyday_joke.bot.services import commands
 from everyday_joke.business.subscribe import SubscribeResult, SubscribeUser
 
 router = Router()
+
+
+async def subscribe(
+    user_id: int,
+    subscribe_user: SubscribeUser,
+) -> str:
+    result = await subscribe_user(user_id)
+
+    if result == SubscribeResult.subscribed:
+        text = locales.en.SUBSCRIBE_SUCCESS
+    elif result == SubscribeResult.already_subscribed:
+        text = locales.en.ALREADY_SUBSCRIBED
+
+    return text
 
 
 @router.callback_query(
@@ -13,21 +29,29 @@ router = Router()
     F.from_user.as_("user"),
     F.message.as_("message"),
 )
-async def subscribe(
+async def subscribe_call(
     call: CallbackQuery,
     message: Message,
     user: User,
     subscribe_user: SubscribeUser,
 ) -> None:
-    result = await subscribe_user(user.id)
-
-    if result == SubscribeResult.subscribed:
-        text = locales.en.SUBSCRIBE_SUCCESS
-    elif result == SubscribeResult.already_subscribed:
-        text = locales.en.ALREADY_SUBSCRIBED
+    text = await subscribe(user.id, subscribe_user)
 
     await call.answer(
         text=text,
         show_alert=True,
     )
     await message.delete()
+
+
+@router.message(
+    Command(commands.subscribe),
+    F.from_user.as_("user"),
+)
+async def subscribe_command(
+    message: Message, user: User, subscribe_user: SubscribeUser
+) -> None:
+    text = await subscribe(user.id, subscribe_user)
+    await message.delete()
+
+    await message.answer(text)
